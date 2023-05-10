892395b028ad3dbdc4c7abdf65eda7c479a27e39
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-05-10 21:44:24 +0100

    My README

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,7 @@ Cache module
 
 import uuid
 import redis
+from typing import Callable, Union
 
 
 class Cache:
@@ -26,3 +27,32 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) \
+            -> Union[str, bytes, int, float]:
+        """
+        Retrieve the data stored at the given key in Redis.
+        If a fn callable is provided, use it to convert the
+        data back to the desired format.
+        Return the data.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            data = fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieve the data stored at the given key in Redis as a string.
+        Return the data as a string.
+        """
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieve the data stored at the given key in Redis as an integer.
+        Return the data as an integer.
+        """
+        return self.get(key, fn=lambda x: int(x))
